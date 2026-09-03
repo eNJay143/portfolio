4c295b1dc7fc77e3341c62573acdd7ee1f47ab45
Norman John Bandibas <24-1-00505@vsu.edu.ph>
2026-09-03 19:54:13 +0800

    [feat] Add Education Section

--- src/features/education/educationData.ts
+++ src/features/education/educationData.ts
@@ -1,1 +1,15 @@
-export const educationData = [];
+export interface EducationItem {
+  id: string;
+  degree: string;
+  university: string;
+  training: string[];
+}
+
+export const educationData: EducationItem[] = [
+  {
+    id: "edu-1",
+    degree: "Bachelor of Science in Computer Science",
+    university: "Visayas State University Main Campus",
+    training: ["START x DOST DataCamp Scholar"]
+  }
+];
